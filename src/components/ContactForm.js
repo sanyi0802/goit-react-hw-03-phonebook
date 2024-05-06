227c5423fdcf63fa3e81cbd8513765094034eff6
Sandra <42647088+sanyi0802@users.noreply.github.com>
2024-05-06 03:54:47 -0500

    Tarea

---
+++ src/components/ContactForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const ContactForm = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddContact({ name, number });
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Number:
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+};
+
+export default ContactForm;
